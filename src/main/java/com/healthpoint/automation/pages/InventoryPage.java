aca28723ede55894b62af8136684f1e6f5f24e8c
Oleh Hordiichuk <oleh.hordiichuk@yahoo.com>
2026-08-27 16:03:26 -0700

    Stabilize Chrome logout interaction
    Use a deterministic JavaScript click after the sidebar is fully rendered to eliminate the remaining Chrome logout flake.

--- src/main/java/com/healthpoint/automation/pages/InventoryPage.java
+++ src/main/java/com/healthpoint/automation/pages/InventoryPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -64,13 +63,13 @@ public class InventoryPage {
         waitForMenuToBeFullyOpen();
 
         WebElement logout = wait.until(
-                ExpectedConditions.elementToBeClickable(logoutLink)
+                ExpectedConditions.visibilityOfElementLocated(logoutLink)
         );
 
-        new Actions(driver)
-                .moveToElement(logout)
-                .click()
-                .perform();
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();",
+                logout
+        );
 
         return new LoginPage(driver).waitUntilLoaded();
     }
